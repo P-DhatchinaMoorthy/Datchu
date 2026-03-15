4ef53248fe60d021c90839fdab08050dad4d9424
p-dhatchinamoorthy <datchu15052003@gmail.com>
2026-03-16 01:48:41 +0530

    Update homepage URL and adjust deployment script in package.json; change base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // <-- Important: your repo name, case-sensitive
+  base: '/P-DhatchinaMoorthy/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
