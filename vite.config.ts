681a9b42bf8e7788f951a86386d5babcc8defb37
p-dhatchinamoorthy <datchu15052003@gmail.com>
2026-03-16 01:55:27 +0530

    Main

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/P-DhatchinaMoorthy/',
+  base: './',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
